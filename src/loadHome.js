485e8dfa976122476cc86c51142f8e677851da82
ShadowVectorZ <rayaudet19@gmail.com>
2024-06-24 13:28:07 -0400

    added picture and header elements and appended to header div

--- src/loadHome.js
+++ src/loadHome.js
@@ -1,8 +1,15 @@
-import baratie from './images/download.jpeg'
+import baratie from './images/Baratie_Before_Timeskip.jpg'
 export function loadHome(){
+    const baratiePic=document.createElement('img')
+    baratiePic.classList.add('baratie')
+    baratiePic.setAttribute('src',baratie)
+    content.appendChild(baratiePic)
+
     const header=document.createElement('h1')
     header.textContent='Eat at Baratie!'
     content.appendChild(header)
+
+    
 
 
 
