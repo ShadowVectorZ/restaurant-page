8d2e198060579b18cd85f2385adcaee71a88e455
ShadowVectorZ <rayaudet19@gmail.com>
2024-06-24 10:23:45 -0400

    created module for home page

---
+++ src/loadHome.js
@@ -0,0 +1,9 @@
+import baratie from './images/download.jpeg'
+export function loadHome(){
+    const header=document.createElement('h1')
+    header.textContent='Eat at Baratie!'
+    content.appendChild(header)
+
+
+
+}
