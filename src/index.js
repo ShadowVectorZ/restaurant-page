fceef5384ddd4c4b126928216f1fe713968dc13e
ShadowVectorZ <rayaudet19@gmail.com>
2024-06-25 15:09:35 -0400

    added code that switchs between pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
 
 
 import { loadHome } from './loadHome.js'
-
+import { loadAbout } from './loadAbout.js'
 
 import './styles.css';
 
+const content=document.querySelector('#content')
 const homeButton=document.querySelector('#home')
+const menuButton=document.querySelector('#menu')
+const aboutButton=document.querySelector('#about')
+
+
 homeButton.addEventListener('click',() =>{
+    clearPage()
     loadHome()
     homeButton.disabled=true
+    menuButton.disabled=false
+    aboutButton.disabled=false
 })
 
-loadHome()
-homeButton.disabled=true
+
+
+
+aboutButton.addEventListener('click',() =>{
+    clearPage()
+    loadAbout()
+    homeButton.disabled=false
+    menuButton.disabled=false
+    aboutButton.disabled=true
+})
+
+let clearPage=function(){
+    while( content.hasChildNodes() ){
+        content.removeChild(content.lastChild);} 
+    }
+
+    loadHome()
+    homeButton.disabled=true
