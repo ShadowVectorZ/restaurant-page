9e6a25818e63174523643ce95d1438a58a287388
ShadowVectorZ <rayaudet19@gmail.com>
2024-06-26 10:53:51 -0400

    added text boxes for each employee

--- src/loadAbout.js
+++ src/loadAbout.js
@@ -10,25 +10,50 @@ export function loadAbout(){
     zeffPic.setAttribute('src',zeff)
     content.appendChild(zeffPic)
 
+
+    const zeffP=document.createElement('p')
+    zeffP.classList.add('employeesText')
+    zeffP.textContent='The captain of the ship, Zeff!'
+    content.appendChild(zeffP)
+
     const sanjiPic=document.createElement('img')
     sanjiPic.classList.add('employees')
     sanjiPic.setAttribute('src',sanji)
     content.appendChild(sanjiPic)
+
+    const sanjiP=document.createElement('p')
+    sanjiP.classList.add('employeesText')
+    sanjiP.textContent='Sanji is the sous chef.'
+    content.appendChild(sanjiP)
+
 
     const pattyPic=document.createElement('img')
     pattyPic.classList.add('employees')
     pattyPic.setAttribute('src',patty)
     content.appendChild(pattyPic)
 
+    const pattyP=document.createElement('p')
+    pattyP.classList.add('employeesText')
+    pattyP.textContent='One of our main cooks, Patty is our head patissier!'
+    content.appendChild(pattyP)
+
     const carnePic=document.createElement('img')
     carnePic.classList.add('employees')
     carnePic.setAttribute('src',carne)
     content.appendChild(carnePic)
+
+    const carneP=document.createElement('p')
+    carneP.classList.add('employeesText')
+    carneP.textContent='Carne is our head charcuttier! (go figure)'
+    content.appendChild(carneP)
 
     const luffyPic=document.createElement('img')
     luffyPic.classList.add('employees')
     luffyPic.setAttribute('src',luffy)
     content.appendChild(luffyPic)
 
-    
+    const luffyP=document.createElement('p')
+    luffyP.classList.add('employeesText')
+    luffyP.textContent='Meet our newest member, Chore Boy!'
+    content.appendChild(luffyP)
 }
