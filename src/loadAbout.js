9795faa648cc1eff0d62d79192f36680c43b8108
ShadowVectorZ <rayaudet19@gmail.com>
2024-06-26 10:13:12 -0400

    added multiple pictures to content display

--- src/loadAbout.js
+++ src/loadAbout.js
@@ -1,14 +1,34 @@
-import baratie from './images/Baratie_Before_Timeskip.jpg'
+import zeff from './images/Zeff.jpg'
+import patty from './images/patty.jpg'
+import carne from './images/carne.jpeg'
+import sanji from './images/sanji.jpg'
+import luffy from './images/chore-boy.jpg'
 export function loadAbout(){
-   
-    const header=document.createElement('h1')
-    header.textContent='Eat at Baratie!'
-    content.appendChild(header)
-    
-    const baratiePic=document.createElement('img')
-    baratiePic.classList.add('baratie')
-    baratiePic.setAttribute('src',baratie)
-    content.appendChild(baratiePic)
+
+    const zeffPic=document.createElement('img')
+    zeffPic.classList.add('employees')
+    zeffPic.setAttribute('src',zeff)
+    content.appendChild(zeffPic)
+
+    const sanjiPic=document.createElement('img')
+    sanjiPic.classList.add('employees')
+    sanjiPic.setAttribute('src',sanji)
+    content.appendChild(sanjiPic)
+
+    const pattyPic=document.createElement('img')
+    pattyPic.classList.add('employees')
+    pattyPic.setAttribute('src',patty)
+    content.appendChild(pattyPic)
+
+    const carnePic=document.createElement('img')
+    carnePic.classList.add('employees')
+    carnePic.setAttribute('src',carne)
+    content.appendChild(carnePic)
+
+    const luffyPic=document.createElement('img')
+    luffyPic.classList.add('employees')
+    luffyPic.setAttribute('src',luffy)
+    content.appendChild(luffyPic)
 
     
 }
